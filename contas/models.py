07f5d7bc47b2e95660d0faab594ed608431dfbd4
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2019-12-26 14:58:06 -0300

    Criaçao da model Transaçao

--- contas/models.py
+++ contas/models.py
@@ -6,4 +6,18 @@ class Categoria(models.Model):
 
     def __str__(self):
         return self.nome
+
+class Transacao(models.Model):
+    data = models.DateTimeField()
+    descricao = models.CharField(max_length=200)
+    valor = models.DecimalField(max_digits=7, decimal_places=2)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    observacoes = models.TextField(null=True, blank=True)
+
+    class Meta:
+        verbose_name_plural = 'Transações'
+
+    def __str__(self):
+        return self.descricao
+    
     

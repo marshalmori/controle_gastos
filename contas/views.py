d4a85c18af03d2e8c123e1568bf5d23da3d6ec4f
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2019-12-26 14:15:20 -0300

    Criaçao do template na app contas

--- contas/views.py
+++ contas/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import datetime 
 
-# Create your views here.
+def home(request):
+    now = datetime.datetime.now()
+    return render(request,'contas/home.html')
+
